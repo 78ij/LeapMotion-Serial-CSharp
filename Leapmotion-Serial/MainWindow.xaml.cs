e836ebb4368e4604cd064b018021042c7316016b
Unknown <1084714805@qq.com>
2018-04-03 20:41:57 +0800

    add SerialPort
    little modifacation

--- Leapmotion-Serial/MainWindow.xaml.cs
+++ Leapmotion-Serial/MainWindow.xaml.cs
@@ -27,12 +27,20 @@ namespace Leapmotion_Serial
     {
         float xratio = 1.70f; // x放大系数
         float zratio = 3.40f; // z放大系数
+        float yratio = 1.00f; // y放大系数
+
+        float depthMax = 1000.0f;
+        float depthMin = 50.0f;
+
         Byte[,] LEDs = new Byte[8, 8];
         List<Label> labels = new List<Label>();
         List<Rectangle> rects = new List<Rectangle>();
+
+        PortConnector port = new PortConnector();
+
         public LeapListener(Label[] label, Grid g)
         {
-            foreach(Label l in label)
+            foreach (Label l in label)
             {
                 labels.Add(l);
             }
@@ -52,7 +60,8 @@ namespace Leapmotion_Serial
                     LEDs[i, j] = 0;
                 }
             }
-            foreach( Rectangle r in rects){
+            foreach (Rectangle r in rects)
+            {
                 r.Fill = Brushes.White;
             }
         }
@@ -69,12 +78,27 @@ namespace Leapmotion_Serial
         {
             a.x *= xratio;
             a.z *= zratio;
+            a.y *= yratio;
             return a;
+        }
+
+        public void tryConnectPort()
+        {
+            if (port.IsPortOpen())
+                return;
+            try
+            {
+                port.OpenPort();
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.Message);
+            }
         }
 
         public void OnInit(Controller controller)
         {
-
+            tryConnectPort();
         }
         public void OnConnect(object sender, DeviceEventArgs args)
         {
@@ -92,51 +116,69 @@ namespace Leapmotion_Serial
             InitLED();
 
             string message = String.Format(
-              "Frame id: {0}, timestamp: {1}, hands: {2}",
+              "PortState: {0}\nFrame id: {1}, timestamp: {2}, hands: {3}",
+              port.IsPortOpen().ToString(),
               frame.Id, frame.Timestamp, frame.Hands.Count
             );
             labels[0].Content = message;
             foreach (Hand hand in frame.Hands)
             {
-                message = String.Format("  Hand id: {0}, palm position: {1}, fingers: {2}, width:{3}",
-                  hand.Id, Transform(hand.PalmPosition), hand.Fingers.Count, hand.PalmWidth * xratio);
-                labels[1].Content = message;
-
                 // Get the hand's normal vector and direction
                 Vector normal = hand.PalmNormal;
                 Vector direction = hand.Direction;
-                Vector point = hand.PalmPosition;
+                Vector position = Transform(hand.PalmPosition);
                 double width = hand.PalmWidth;
                 // Calculate the hand's pitch, roll, and yaw angles
-                message = String.Format(
-                  "  Hand pitch: {0} degrees, roll: {1} degrees, yaw: {2} degrees",
+
+                message = String.Format("Hand id: {0}, palm position: {1}, fingers: {2}, width:{3}\nHand pitch: {0} degrees, roll: {1} degrees, yaw: {2} degrees",
+                  hand.Id,
+                  hand.PalmPosition, 
+                  hand.Fingers.Count, 
+                  hand.PalmWidth * xratio,
+
+                  "",
                   direction.Pitch * 180.0f / (float)Math.PI,
                   normal.Roll * 180.0f / (float)Math.PI,
                   direction.Yaw * 180.0f / (float)Math.PI
+                  );
+                labels[1].Content = message;
+
+                
+                float left = position.x - hand.PalmWidth / 2 * xratio;
+                float right = left + hand.PalmWidth * xratio;
+                float top = position.z + hand.PalmWidth / 2 * zratio;
+                float down = top - hand.PalmWidth * zratio;
+                float depth = position.y;
+                byte color = (byte)(255 * Math.Max(Math.Min((depth - depthMin) / (depthMax - depthMin), 1.0f), 0.0f));
+
+
+                message = String.Format(
+                  "palm position scaled: {0}\nplane pos: ({1}, {2}), depth: {3}\ncolor: {4}",
+                  position, (int)Math.Round(position.x), (int)Math.Round(position.z), (int)Math.Round(position.y),
+                  color
                 );
                 labels[2].Content = message;
-                float z = 0;
-                float left = Transform(hand.PalmPosition).x - hand.PalmWidth / 2 * xratio;
-                float right = left + hand.PalmWidth * xratio;
-                float top = Transform(hand.PalmPosition).z + hand.PalmWidth / 2 * zratio;
-                float down = top - hand.PalmWidth * zratio;
-                for(int i = 0;i <= 7; i++)
+
+                for (int i = 0; i <= 7; i++)
                 {
-                    for(int j = 0;j <= 7; j++)
+                    for (int j = 0; j <= 7; j++)
                     {
-                        if (IsInRect(new Vector(85 * j - 340, 0, 85 * i - 340),left,right,top,down)){
-                            LEDs[i,j] = 1;
-                            rects[i * 8 + j].Fill = Brushes.Black;
+                        if (IsInRect(new Vector(85 * j - 340, 0, 85 * i - 340), left, right, top, down))
+                        {
+                            LEDs[i, j] = 1;
+                            rects[i * 8 + j].Fill = new SolidColorBrush(Color.FromRgb(color, color, color));
                         }
                     }
                 }
             }
+
+            port.Write(LEDs);
         }
 
     }
     public partial class MainWindow : Window
     {
-       
+
 
         public MainWindow()
         {
@@ -144,8 +186,8 @@ namespace Leapmotion_Serial
             using (Leap.IController controller = new Leap.Controller())
             {
                 controller.SetPolicy(Leap.Controller.PolicyFlag.POLICY_ALLOW_PAUSE_RESUME);
-                Label[] l= new Label[] { label1, label2, label3 };
-                LeapListener listener = new LeapListener(l,Rects);
+                Label[] l = new Label[] { label1, label2, label3 };
+                LeapListener listener = new LeapListener(l, Rects);
                 controller.Device += listener.OnConnect;
                 controller.Disconnect += listener.OnDisconnect;
                 controller.FrameReady += listener.OnFrame;
---
+++ Leapmotion-Serial/PortConnector.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace Leapmotion_Serial
+{
+    class PortConnector
+    {
+
+        private SerialPort device = new SerialPort();
+
+        public void OpenPort()
+        {
+            device.PortName = "";               // 端口名
+            device.BaudRate = 60 * 66 * 8;      // 波特率：60次刷新/秒 * 66bytes/一次刷新 * 8bits/byte
+            device.Parity = Parity.None;        // 奇偶校验
+            device.DataBits = 8;                // 8位的数据位
+            device.StopBits = StopBits.One;     // 使用1个停止位
+            device.Handshake = Handshake.None;  // 握手协议：待定
+
+            device.ReadTimeout = 500;           // 读取超时：500ms
+            device.WriteTimeout = 500;          // 写入超时：500ms
+
+            try
+            {
+                device.Open();
+            } catch
+            {
+                // TO-DO:打开端口失败处理
+                throw;
+            }
+
+            device.DataReceived += new SerialDataReceivedEventHandler(DataReceiveHandler);
+            device.ErrorReceived += new SerialErrorReceivedEventHandler(ErrorHandler);
+        }
+
+        public void ClosePort()
+        {
+            device.Close();
+        }
+
+        public bool IsPortOpen()
+        {
+            return device.IsOpen;
+        }
+
+        public void Write(byte[,] state)
+        {
+            if (!device.IsOpen)
+                return;
+
+            if (state.GetLength(0) != 8 || state.GetLength(1) != 8)
+            {
+                throw new Exception("Data Size not compatible!");
+            }
+
+            byte[] bytes = new byte[64];
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    bytes[i * 8 + j] = state[i, j];
+                }
+            }
+
+            try
+            {
+                device.Write(bytes, 0, bytes.Length);
+            } catch
+            {
+                // TO-DO:发送失败处理
+                //throw;
+            }
+        }
+
+        private void DataReceiveHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] ReDatas = new byte[device.BytesToRead];
+            device.Read(ReDatas, 0, ReDatas.Length);
+            // TO-DO:获得数据之后
+        }
+
+        private void ErrorHandler(object sender, SerialErrorReceivedEventArgs e)
+        {
+            // TO-DO:错误处理
+        }
+    }
+}
